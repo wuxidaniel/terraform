7b6d4632313a2a72d6082fa9d98a424e8b848372
Daniel <qianxingnan@gmail.com>
2026-03-31 23:38:56 -0400

    nest route in route table ngw_resources.tf

--- ngw_resources.tf
+++ ngw_resources.tf
@@ -15,10 +15,11 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
+  route {
+	  cidr_block = "0.0.0.0/0"
+	  nat_gateway_id         = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "up247-private-subnet-routes"
+  }
 }
